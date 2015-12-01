929d79653932617814bcf3e798e3465c128de6e4
gmankd <pabloguzman26@yahoo.com>
2015-12-01 09:35:57 -0500

    Added the js links to html page

--- scripts/click-handlers.js
+++ scripts/click-handlers.js
@@ -1,13 +1,29 @@
 $(document).ready(function(){
 
 //Landing Page Display
-$("#login-form").click(function() {
-    $(".holiday-view").show();
+$("#login-page-buttons").click(function() {
     // $("#logout-button").show();
-    $("#registration-form").hide();
-    $("#login-form").hide();
-    $('#logged-in').hide();
-    $('#registration-complete').hide();
+    $("#login-button-text").hide();
     $('#login-complete').show();
   });
+$("#login-page-buttons").click(function() {
+    // $("#logout-button").show();
+    $("#login-button-text").hide();
+    $('#login-complete').show();
+  });
+
+ $('#register').on('submit', function(e) {
+        var credentials = wrap('credentials', form2object(this));
+        weather_api.register(credentials, function(err, data){
+          handleError(err, data, function(){
+            alert("Invalid registration");
+          });
+          $('#register_form').hide();
+          $('#login_form').css('margin', '0px auto');
+          $('#spacer').addClass('col-xs-3');
+        });
+        e.preventDefault();
+      });
+
+
 });
