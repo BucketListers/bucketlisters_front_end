89accabec5f2fe7c3105fa6b4c3d2377b56f74d6
gmankd <pabloguzman26@yahoo.com>
2015-12-01 14:59:21 -0500

    fixed issue when pushing to development

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var weather_api = {
+var bucketList_api = {
     //url: 'https://someurl',
-    url: 'http://localhost:3000',
+   // url: 'http://localhost:3000',
+    url: 'http://httpbin.org/',
 
     ajax: function(config, cb) {
         $.ajax(config).done(function(data, textStatus, jqxhr) {
@@ -46,5 +47,58 @@ var weather_api = {
             dataType: 'json'
         }, callback);
     },
+
+
+    ///// Profile
+    createProfile: function createProfile(profile, user_id, token, callback) {
+        this.ajax({
+            method: 'POST',
+            url: this.url + '/users/' + user_id + '/profile',
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            contentType: 'application/json',
+            data: JSON.stringify(profile),
+            dataType: 'json'
+        }, callback);
+    },
+
+    updateProfile: function updateProfile(profile, user_id, token, callback) {
+        this.ajax({
+            method: 'PATCH',
+            url: this.url + '/users/' + user_id + '/profile',
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            contentType: 'application/json',
+            data: JSON.stringify(profile),
+            dataType: 'json'
+        }, callback);
+    },
+
+    readProfile: function readProfile(user_id, token, callback) {
+        this.ajax({
+            method: 'GET',
+            url: this.url + '/users/' + user_id + '/profile',
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            dataType: 'json'
+
+        }, callback);
+    },
+
+    destroyProfile: function destroyProfile(user_id, token, callback) {
+        this.ajax({
+            method: 'DELETE',
+            url: this.url + '/users/' + user_id + '/profile',
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            dataType: 'json'
+
+        }, callback);
+    },
+
 
 };
