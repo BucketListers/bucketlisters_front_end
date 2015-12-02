93e996262b0aaa3d7bc266acd2e5a8563f9d6673
gmankd <pabloguzman26@yahoo.com>
2015-12-02 09:12:08 -0500

    PG DH Added CRUD for list items.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -2,7 +2,7 @@
 
 var bucketList_api = {
     //url: 'https://someurl',
-   url: 'http://localhost:3000',
+    url: 'http://localhost:3000',
     // url: 'http://httpbin.org/',
 
     ajax: function(config, cb) {
@@ -20,13 +20,10 @@ var bucketList_api = {
     signup: function register(credentials, callback) {
         this.ajax({
             method: 'POST',
-            // xhrFields: {
-            //     withCredentials: true
-            // },
             url: this.url + '/signup',
             contentType: 'application/json',
             data: JSON.stringify(credentials)
-            }, callback);
+        }, callback);
     },
 
     login: function login(credentials, callback) {
@@ -49,58 +46,55 @@ var bucketList_api = {
             xhrFields: {
                 withCredentials: true
             }
-            //dataType: 'json'
         }, callback);
     },
 
 
     ///// Profile
-    createProfile: function createProfile(profile, user_id, token, callback) {
+    createListItem: function createProfile(createlistItem, callback) {
         this.ajax({
             method: 'POST',
-            url: this.url + '/users/' + user_id + '/profile',
-            headers: {
-                Authorization: 'Token token=' + token
-            },
+            url: this.url + '/items',
             contentType: 'application/json',
-            data: JSON.stringify(profile),
+            data: JSON.stringify(listItem),
+            xhrFields: {
+                withCredentials: true
+            },
             dataType: 'json'
         }, callback);
     },
 
-    updateProfile: function updateProfile(profile, user_id, token, callback) {
+    updateListItem: function updateListItem(item_id, callback) {
         this.ajax({
             method: 'PATCH',
-            url: this.url + '/users/' + user_id + '/profile',
-            headers: {
-                Authorization: 'Token token=' + token
-            },
-            contentType: 'application/json',
-            data: JSON.stringify(profile),
-            dataType: 'json'
+            url: this.url + '/items/' + item_id,
+            dataType: 'json',
+            xhrFields: {
+                withCredentials: true
+            }
         }, callback);
     },
 
-    readProfile: function readProfile(user_id, token, callback) {
+    showList: function showList(callback) {
         this.ajax({
             method: 'GET',
-            url: this.url + '/users/' + user_id + '/profile',
-            headers: {
-                Authorization: 'Token token=' + token
-            },
-            dataType: 'json'
+            url: this.url + '/items',
+            dataType: 'json',
+            xhrFields: {
+                withCredentials: true
+            }
 
         }, callback);
     },
 
-    destroyProfile: function destroyProfile(user_id, token, callback) {
+    destroyListItem: function destroyListItem(callback) {
         this.ajax({
             method: 'DELETE',
-            url: this.url + '/users/' + user_id + '/profile',
-            headers: {
-                Authorization: 'Token token=' + token
-            },
-            dataType: 'json'
+            url: this.url + '/users/' + '/items',
+            dataType: 'json',
+            xhrFields: {
+                withCredentials: true
+            }
 
         }, callback);
     },
